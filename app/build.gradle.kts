e02a04d02eb02be3b44a6bc95955c8eba51be886
Kai Eysselein <kaieysselein@gmail.com>
2026-08-03 20:07:58 +0200

    Release DataNudge 0.2.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kaieysselein.datanudge"
         minSdk = 26
         targetSdk = 37
-        versionCode = 20301
-        versionName = "0.2.3.1"
+        versionCode = 20302
+        versionName = "0.2.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
